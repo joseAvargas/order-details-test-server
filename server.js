3dd2b6bc182e0cfaa6cfe846acde9089c58cd09f
Vargas <Jose.Vargas@ingrammicro.com>
2021-09-22 22:51:05 -0700

    updating db

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 4000;
 
-// server.use(function(req, res, next) {
-//     setTimeout(next, 3000);
-// })
+server.use(function(req, res, next) {
+    setTimeout(next, 1000);
+})
 server.use(middlewares)
 server.use(router)
 server.listen(port, () => {
